7e6a3b877f39ef422d145976a316a40a6be61676
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-09-15 14:59:44 -0400

    added interface for work flow

--- Assignment One/Palindromes/src/main/SelfStack.java
+++ Assignment One/Palindromes/src/main/SelfStack.java
@@ -1,5 +1,64 @@
 package main;
 
-public class SelfStack {
+interface FullStack<S> {
+    void push(S value);
+    S pop();
+    S peek();
+    boolean isEmpty();
+    int size();
+    int search(Object o);
+}
+
+public class SelfStack<S> implements FullStack {
+
+    SelfStack<S> previous;
+    S value;
+
+    SelfStack() {}
+
+    SelfStack(S value) {
+        this.value = value;
+    }
+
+    SelfStack(SelfStack<S> previous, S value) {
+        this.previous = previous;
+        this.value = value;
+    }
+
+    @Override
+    public void push(Object value) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Object pop() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int search(Object o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
     
 }
