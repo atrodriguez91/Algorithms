7679bfc8eea62a1045ad37a4212c5e2747c82fd8
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-09-15 13:04:00 -0400

    Init node, init head and tail, init item

--- Assignment One/src/Node.java
+++ Assignment One/src/Node.java
@@ -4,5 +4,18 @@ public class Node {
 
     String item;
     Node next;
+
+    Node(String item) {
+        this.item = item;
+        this.next = null;
+    }
     
+
+
+public Node head = null;
+public Node tail = null;
+
+public void addPoint(String item) {
+    Node newPoint = new Node(item);
+}
 }
