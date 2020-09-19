3dbe67c764300965efba26f2d486648541306c71
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-09-18 22:36:12 -0400

    Added comments

--- Assignment One/Palindromes/src/main/Queue.java
+++ Assignment One/Palindromes/src/main/Queue.java
@@ -1,15 +1,16 @@
 package main;
 
 import java.util.ArrayList;
-import java.util.List;
 
+//Class with members to set up properties 
+//of the queue
 public class Queue {
     String[] arr;
     int front;
     int rear;
     int capacity;
     int count;
-
+    //Queue method init 
     Queue(int size) {
         arr = new String[size];
         front = 0;
@@ -17,24 +18,25 @@ public class Queue {
         count = 0;
         capacity = size;
     }
-
+    //Enqueue towards a certain size, parameter is Array
+    // Increases count of queue
     public void enqueue(ArrayList<String> newList) {
         rear = (rear + 1) % capacity;
         arr[rear] = newList.toString();
         count++;
         
     }
-
+    //Dequeues and returns front
     public String dequeue() {
         front = (front + 1) % capacity;
         count--;
         return arr[front];
     }
-
+    //Returns the front of the queue 
     public String peek() {
         return arr[front];
     }
-
+    //Testing purposes 
     public static void main(String[] args) {
         Queue q = new Queue(3);
 
