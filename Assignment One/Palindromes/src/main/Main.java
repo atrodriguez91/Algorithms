4d21c643ea88e12735ce388762adf10666dfac4b
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-09-18 22:25:41 -0400

    Change to main, added comments

--- Assignment One/Palindromes/src/main/Main.java
+++ Assignment One/Palindromes/src/main/Main.java
@@ -2,32 +2,32 @@ package main;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
-import main.SelfStack;
 
-public class Read {
+
+public class Main {
 
     public static void main(String[] args) throws IOException{
+        //Variable to store String
         String line = "";
 
-        //Scan the file
+        //Scan the file using a scanner for data capture
         Scanner file = new Scanner (new File("magicitems.txt"));
         
-
+        //Place the contents of the data into an array
         List<String> newList = new ArrayList<String>();
 
-
+        //While loop to cycle through data and store into "line" variable
+        //Adds the lines into a newList
         while (file.hasNext()) {
             line = file.next();
             newList.add(line);
         }
 
         file.close();
-
+        // Creation of stack and queue instance 
         SelfStack<String> stack = new SelfStack<String>();
         stack.push(newList);
         Queue q = new Queue(100);
