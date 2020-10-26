de9d724bd7cf810e385e3c5690fe2bc579c99c68
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-10-26 16:06:00 -0400

    File reader

---
+++ Assignment Three/Search.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Search {
+    static String[] magicArray;
+    public static String[] findFile() throws FileNotFoundException {
+        Scanner magicScan = new Scanner(new File("magicitems.txt"));
+        ArrayList<String> magicList = new ArrayList<String>();
+        while (magicScan.hasNext()) {
+            magicList.add(magicScan.nextLine());
+        }
+        magicArray = magicList.toArray(new String[0]);
+        return magicArray;
+
+
+    }
+}
