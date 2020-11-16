3c46bc9e3ab14cc9f7fb6aa9197cbf720592876c
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-11-16 07:02:57 -0500

    Vertex for each graph

--- Assignment Four/Graph/src/InfoRead.java
+++ Assignment Four/Graph/src/InfoRead.java
@@ -3,6 +3,7 @@ package src;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InfoRead {
@@ -35,4 +36,42 @@ public class InfoRead {
         }
         return vertexIntList;  
     }  
+    // VERTEX FOR EACH GRAPH (Only way I could make it work)
+    public List<Integer> firstGraphV(ArrayList<Integer> list){
+        List<Integer> firstGraphVL = new ArrayList<Integer>();
+        for (int i = 0; i < 7; i++) {
+            firstGraphVL.add(list.get(i));
+        }
+        return firstGraphVL;
+    }
+
+    public List<Integer> secondGraphV(ArrayList<Integer> list) {
+        ArrayList<Integer> secondGraphVL = new ArrayList<Integer>();
+        for (int i = 7; i < 15; i++) {
+            secondGraphVL.add(list.get(i));
+        }
+        return secondGraphVL;
+    }
+    public ArrayList<Integer> thirdGraphV(ArrayList<Integer> list){
+        ArrayList<Integer> thirdGraphVL = new ArrayList<Integer>();
+        for (int i = 15; i < 78; i++) {
+            thirdGraphVL.add(list.get(i));
+        }
+        return thirdGraphVL;
+    }
+    public ArrayList<Integer> fourthGraphV(ArrayList<Integer> list){
+        ArrayList<Integer> fourthGraphVL = new ArrayList<Integer>();
+        for (int i = 78; i < 142; i++) {
+            fourthGraphVL.add(list.get(i));
+        }
+        return fourthGraphVL;
+    }
+    public ArrayList<Integer> fifthGraphV(ArrayList<Integer> list){
+        ArrayList<Integer> fifthGraphVL = new ArrayList<Integer>();
+        for (int i = 142; i < 163; i++) {
+            fifthGraphVL.add(list.get(i));
+        }
+        return fifthGraphVL;
+    }
+    //================================================================
 }
