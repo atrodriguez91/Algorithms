189dd1f7edbf7df110170a193f1f4a9252658890
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-11-16 07:45:56 -0500

    Comments and print statement

--- Assignment Four/Graph/src/AdjacentList.java
+++ Assignment Four/Graph/src/AdjacentList.java
@@ -1,16 +1,20 @@
 package src;
 
 import java.util.ArrayList;
-
+// Similar to matrix, using a 2D arraylist to represent the graph.
 public class AdjacentList {
     public static void addEdge(ArrayList<ArrayList<Integer>> adj, Integer i, Integer j) {
         adj.get(i).add(j);
         adj.get(j).add(i);
     }
-
+    // Printer of the list. 
     public static void printList(ArrayList<ArrayList<Integer>> adj) {
         for (int i = 0; i < adj.size(); i++) {
-            
+            System.out.println(i);
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.println(" => " + adj.get(i).get(j));
+            }
+            System.out.println();
         }
     }
 
