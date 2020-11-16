c81c9ebdef77c7fe2582331badae875de646d3cf
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-11-16 07:47:44 -0500

    First adj graph, formatting

--- Assignment Four/Graph/src/AdjacentList.java
+++ Assignment Four/Graph/src/AdjacentList.java
@@ -1,13 +1,17 @@
 package src;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
+
 // Similar to matrix, using a 2D arraylist to represent the graph.
 public class AdjacentList {
     public static void addEdge(ArrayList<ArrayList<Integer>> adj, Integer i, Integer j) {
         adj.get(i).add(j);
         adj.get(j).add(i);
     }
-    // Printer of the list. 
+
+    // Printer of the list.
     public static void printList(ArrayList<ArrayList<Integer>> adj) {
         for (int i = 0; i < adj.size(); i++) {
             System.out.println(i);
@@ -18,7 +22,20 @@ public class AdjacentList {
         }
     }
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        InfoRead r = new InfoRead(); 
+        // FIRST LIST START ========================================================
+        Integer V = r.firstGraphV(r.getVertex(r.findFile())).size() + 1;
+        ArrayList<ArrayList<Integer> > adj = new ArrayList<ArrayList<Integer> >(V);   
+        for (int i = 0; i < V; i++){ 
+            adj.add(new ArrayList<Integer>()); 
+        }
+        List<Integer> a = r.firstGraphE(r.getEdgesFirst(r.findFile()));
+        List<Integer> b = r.firstGraphE(r.getEdgesSecond(r.findFile()));
+        for (int i = 0; i < 11; i++) {
+          addEdge(adj, a.get(i), b.get(i));
+        } 
+        printList(adj);
+        // END OF FIRST LIST =======================================================
     }
 }
