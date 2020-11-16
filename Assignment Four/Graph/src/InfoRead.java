f42a5302a37a0419b87e8b384c148a0a6166994b
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-11-16 07:07:34 -0500

    Two functions that collect src and dest of edges

--- Assignment Four/Graph/src/InfoRead.java
+++ Assignment Four/Graph/src/InfoRead.java
@@ -5,11 +5,15 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InfoRead {
 
     String[] graphFile;
+    String[] edges;
     int vertexInt;
+    ArrayList<String> edgeIntList = new ArrayList<String>();
+    ArrayList<String> edgeIntList1 = new ArrayList<String>();
     ArrayList<Integer> vertexIntList = new ArrayList<Integer>();
 
     public String[] findFile() throws FileNotFoundException {
@@ -74,4 +78,31 @@ public class InfoRead {
         return fifthGraphVL;
     }
     //================================================================
+    // This collects the first source edges of the file. 
+    public List<String> getEdgesFirst(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i].contains("add edge")) {
+             arr[i] = arr[i].replaceAll("[^0-9]", " ").trim();
+             edges = new String[arr.length];
+             edges[i] = (String) arr[i].subSequence(0, 2);
+             edgeIntList.add(edges[i]);
+            }
+        }
+        List<String> trim1 = edgeIntList.stream().map(String::trim).collect(Collectors.toList());
+        return trim1;
+    }
+    // Collects the destination edges of the file. 
+    public List<String> getEdgesSecond(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i].contains("add edge")) {
+             arr[i] = arr[i].replaceAll("[^0-9]", "  ").trim();
+             edges = new String[arr.length];
+             edges[i] = (String) arr[i].substring(4);
+             edgeIntList1.add(edges[i]);
+            }
+        }
+        List<String> trim = edgeIntList1.stream().map(String::trim).collect(Collectors.toList());
+        return trim;
+    }
+    //==============================================================================================
 }
