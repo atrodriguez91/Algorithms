d0ad9ab445a72f237887c34ead224ae54aec74eb
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-11-16 07:22:28 -0500

    Matrix for graph 1

--- Assignment Four/Graph/src/GraphMatrix.java
+++ Assignment Four/Graph/src/GraphMatrix.java
@@ -1,5 +1,8 @@
 package src;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class GraphMatrix {
     public boolean adjMatrix[][];
     public Integer vertices;
@@ -12,5 +15,31 @@ public class GraphMatrix {
     public void addEdge(Integer i, Integer j) {
         adjMatrix[i][j] = true;
         adjMatrix[j][i] = true;
+    }
+
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i < vertices; i++) {
+            string.append(" : ");
+            for (boolean j : adjMatrix[i]) {
+                string.append((j ? 1 : 0) + " ");
+            }
+            string.append("\n");
+        }
+        return string.toString();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // MATRIX FOR FIRST GRAPH ====================================================================
+        InfoRead read = new InfoRead();
+        GraphMatrix m = new GraphMatrix(read.firstGraphV(read.getVertex(read.findFile())).size() + 1);
+        List<Integer> a = read.firstGraphE(read.getEdgesFirst(read.findFile()));
+        List<Integer> b = read.firstGraphE(read.getEdgesSecond(read.findFile()));
+        for (int i = 0; i < 11; i++) {
+            m.addEdge(a.get(i), b.get(i));
+        }
+        System.out.println(m.toString());
+        // END OF GRAPH 1 ============================================================================
+        
     }
 }
