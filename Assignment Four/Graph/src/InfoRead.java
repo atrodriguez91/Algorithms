9ebfba4d790d80000e99e9c24c86dbd7586df11f
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-11-16 07:00:03 -0500

    Vertices array

--- Assignment Four/Graph/src/InfoRead.java
+++ Assignment Four/Graph/src/InfoRead.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 public class InfoRead {
 
     String[] graphFile;
+    int vertexInt;
+    ArrayList<Integer> vertexIntList = new ArrayList<Integer>();
 
     public String[] findFile() throws FileNotFoundException {
         Scanner graphScan = new Scanner(new File("graph1.txt"));
@@ -19,5 +21,18 @@ public class InfoRead {
         return graphFile;
 
     }
-    
+
+    //GETTING VERTEX INFORMATION
+    // Compile ALL vertices from the file. 
+
+    public ArrayList<Integer> getVertex(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i].contains("add vertex")){
+                arr[i] = arr[i].replaceAll("[^0-9]", " ").trim();
+                vertexInt = Integer.parseInt(arr[i]);
+                vertexIntList.add(vertexInt);
+            }
+        }
+        return vertexIntList;  
+    }  
 }
