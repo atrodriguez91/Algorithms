a31c244a1c8af68da8e38ec6aedaff65aed10cb8
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-11-16 07:27:55 -0500

    Complete matrices for remaining graphs

--- Assignment Four/Graph/src/GraphMatrix.java
+++ Assignment Four/Graph/src/GraphMatrix.java
@@ -40,6 +40,45 @@ public class GraphMatrix {
         }
         System.out.println(m.toString());
         // END OF GRAPH 1 ============================================================================
-        
+        // MATRIX FOR SECOND GRAPH ===================================================================
+        System.out.println("\n======================================\n");
+        GraphMatrix m1 = new GraphMatrix(read.secondGraphV(read.getVertex(read.findFile())).size() + 1);
+        List<Integer> a1 = read.secondGraphE(read.getEdgesFirst(read.findFile()));
+        List<Integer> b1 = read.secondGraphE(read.getEdgesSecond(read.findFile()));
+        for (int i = 0; i < 28; i++) {
+          m1.addEdge(a1.get(i), b1.get(i));
+        }
+        System.out.println(m1.toString());
+        // END OF GRAPH 2 ============================================================================
+        // MATRIX FOR THIRD GRAPH ====================================================================
+        System.out.println("\n======================================\n");
+        GraphMatrix m2 = new GraphMatrix(read.thirdGraphV(read.getVertex(read.findFile())).size() + 1);
+        List<Integer> a2 = read.thirdGraphE(read.getEdgesFirst(read.findFile()));
+        List<Integer> b2 = read.thirdGraphE(read.getEdgesSecond(read.findFile()));
+        for (int i = 0; i < 62; i++) {
+          m2.addEdge(a2.get(i), b2.get(i));
+        }
+        System.out.println(m2.toString());
+        // END OF GRAPH 3 ============================================================================
+        // MATRIX FOR FOURTH GRAPH ===================================================================
+        System.out.println("\n======================================\n");
+        GraphMatrix m3 = new GraphMatrix(read.fourthGraphV(read.getVertex(read.findFile())).size() + 1);
+        List<Integer> a3 = read.fourthGraphE(read.getEdgesFirst(read.findFile()));
+        List<Integer> b3 = read.fourthGraphE(read.getEdgesSecond(read.findFile()));
+        for (int i = 0; i < 72; i++) {
+            m3.addEdge(a3.get(i), b3.get(i));
+        }
+        System.out.println(m3.toString());
+        // END OF GRAPH 4 ============================================================================
+        // MATRIX FOR FIFTH GRAPH ====================================================================
+        System.out.println("\n======================================\n");
+        GraphMatrix m4 = new GraphMatrix(read.fifthGraphV(read.getVertex(read.findFile())).size() + 1);
+        List<Integer> a4 = read.fifthGraphE(read.getEdgesFirst(read.findFile()));
+        List<Integer> b4 = read.fifthGraphE(read.getEdgesSecond(read.findFile()));
+        for (int i = 0; i < 25; i++) {
+            m4.addEdge(a4.get(i), b4.get(i));
+        }
+        System.out.println(m4.toString());
+        // END OF GRAPH 5=============================================================================
     }
 }
