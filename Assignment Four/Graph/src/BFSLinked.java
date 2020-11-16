2bacd5e96d3f595759e1bf7194c77908ba944988
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-11-16 08:36:00 -0500

    Remaining graphs

--- Assignment Four/Graph/src/BFSLinked.java
+++ Assignment Four/Graph/src/BFSLinked.java
@@ -51,5 +51,54 @@ public class BFSLinked {
 
         System.out.println("BFS Traversal: "); 
         g.BFS(rr.firstGraphV(rr.getVertex(rr.findFile())).get(0)); 
+
+        System.out.println("\n=========================================================\n");
+
+        InfoRead rr1 = new InfoRead();
+        BFSLinked g1 = new BFSLinked(rr1.secondGraphV(rr1.getVertex(rr1.findFile())).size() + 1);
+        List<Integer> a1 = rr1.secondGraphE(rr1.getEdgesFirst(rr1.findFile()));
+        List<Integer> b1 = rr1.secondGraphE(rr1.getEdgesSecond(rr1.findFile()));
+        for (int i = 0; i < 28; i++) {
+          g1.addEdge(a1.get(i), b1.get(i));
+        } 
+
+        System.out.println("BFS Traversal: "); 
+        g1.BFS(rr1.secondGraphV(rr1.getVertex(rr1.findFile())).get(7));
+
+        System.out.println("\n=========================================================\n");
+
+        BFSLinked g2 = new BFSLinked(rr.thirdGraphV(rr1.getVertex(rr1.findFile())).size() + 1);
+        List<Integer> a2 = rr1.thirdGraphE(rr1.getEdgesFirst(rr1.findFile()));
+        List<Integer> b2 = rr1.thirdGraphE(rr1.getEdgesSecond(rr1.findFile()));
+        for (int i = 0; i < 62; i++) {
+          g2.addEdge(a2.get(i), b2.get(i));
+        } 
+
+        System.out.println("BFS Traversal: "); 
+        g2.BFS(rr1.thirdGraphV(rr1.getVertex(rr1.findFile())).get(20)); 
+
+        System.out.println("\n=========================================================\n");
+
+        BFSLinked g3 = new BFSLinked(rr.fourthGraphV(rr1.getVertex(rr1.findFile())).size() + 1);
+        List<Integer> a3 = rr1.fourthGraphE(rr1.getEdgesFirst(rr1.findFile()));
+        List<Integer> b3 = rr1.fourthGraphE(rr1.getEdgesSecond(rr1.findFile()));
+        for (int i = 0; i < 72; i++) {
+          g3.addEdge(a3.get(i), b3.get(i));
+        } 
+
+        System.out.println("BFS Traversal: "); 
+        g3.BFS(rr1.fourthGraphV(rr1.getVertex(rr1.findFile())).get(0));
+
+        System.out.println("\n=========================================================\n");
+
+        BFSLinked g4 = new BFSLinked(rr.fifthGraphV(rr1.getVertex(rr1.findFile())).size() + 1);
+        List<Integer> a4 = rr1.fifthGraphE(rr1.getEdgesFirst(rr1.findFile()));
+        List<Integer> b4 = rr1.fifthGraphE(rr1.getEdgesSecond(rr1.findFile()));
+        for (int i = 0; i < 25; i++) {
+          g4.addEdge(a4.get(i), b4.get(i));
+        } 
+
+        System.out.println("BFS Traversal: "); 
+        g4.BFS(rr1.fifthGraphV(rr1.getVertex(rr1.findFile())).get(0));
     }
 }
