f1c2d3d373a7117e67504ff4d68c73afee132ab3
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-11-16 08:42:04 -0500

    Init of BTS and magicitems.txt

---
+++ Assignment Four/Graph/src/BTS.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BTS {
+
+    String[] magicFile;
+    
+    public String[] findFile() throws FileNotFoundException {
+        Scanner magicScan = new Scanner(new File("magicitems.txt"));
+        ArrayList<String> magicList = new ArrayList<String>();
+        while (magicScan.hasNextLine()) {
+            magicList.add(magicScan.nextLine());
+        }
+        magicFile = magicList.toArray(new String[0]);
+        return magicFile;
+
+    }
+}
+
+    public static void main(String[] args) {
+
+}
