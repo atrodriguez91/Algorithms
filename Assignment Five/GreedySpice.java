aa8cfc1b513b11c64ca60538341c0a9d8f95b5a3
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-12-04 17:08:16 -0500

    fixing functions

--- Assignment Five/GreedySpice.java
+++ Assignment Five/GreedySpice.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class GreedySpice {
     String[] preparedSpice = {""};
     public String[] spiceFinder() throws FileNotFoundException {
@@ -15,52 +16,81 @@ public class GreedySpice {
         return preparedSpice;
     }
 
-    public void findSpiceInfo(String[] preparedSpice) {
-        ArrayList<String> red = new ArrayList<String>();
-        ArrayList<String> green = new ArrayList<String>();
-        ArrayList<String> blue = new ArrayList<String>();
-        ArrayList<String> orange = new ArrayList<String>();
-        String [] test;
-        String totalPrice = "";
+    public String findSpiceInfo(String[] preparedSpice) {
+        String test = "";
+        String test1 = "";
+        String test2 = "";
+        String test3 = "";
         for (int i = 0; i < preparedSpice.length; i++) {
             if(preparedSpice[i].endsWith(";")) {
                 //System.out.println(preparedSpice[i]);
                 if(preparedSpice[i].substring(i).contains("red")) {
-                    red.add(preparedSpice[i]);
-                    test = new String[preparedSpice.length];
-                    test[i] = preparedSpice[i];
-                   
-                    //System.out.println(test[i]);
+                    
+                    //test = preparedSpice[i];
+                    //return test;
                 } else if (preparedSpice[i].substring(i).contains("green")) {
-                    green.add(preparedSpice[i]);
-                    //System.out.println(green);
+                    
+                    test1 = preparedSpice[i];
+                    return test1;
                 } else if (preparedSpice[i].substring(i).contains("blue")) {
-                    blue.add(preparedSpice[i]);
-                    //System.out.println(blue);
+                    
+                    //test2 = preparedSpice[i];
+                    //return test2;
                 } else if (preparedSpice[i].substring(i).contains("orange")) {
-                    orange.add(preparedSpice[i]);
-                    //System.out.println(orange);
+                    
+                    //test3 = preparedSpice[i];
+                    //return test3;
                 }
             }
         }
-        
+        return test1;
         
     }
+    
+    public double findPrice(String string) {
 
-    public ArrayList<String> findKnapsack(String[] preparedSpice) {
-        ArrayList<String> c = new ArrayList<String>();
-        for (int i = 0; i < preparedSpice.length; i++) {
-            if (preparedSpice[i].contains("knapsack ")) {
-                c.add(preparedSpice[i]);
+        String delim = "[ ;]";
+        double totalPrice = 0;
+        String [] price = string.split(delim);
+        for (int i = 0; i < price.length; i++) {
+            if (price[i].contains("total_price")) {
+                //System.out.println(price[i + 2]);
+                totalPrice = Double.parseDouble(price[i + 2]);
             }
         }
-        return c;
+        return totalPrice;
     }
 
+    public int findQty(String string) {
+        String delim = "[ ;]";
+        int totQty = 0;
+        String [] qty = string.split(delim);
+        for (int i = 0; i < qty.length; i++) {
+            if (qty[i].contains("qty")) {
+                totQty = Integer.parseInt(qty[i + 2]);
+            }
+        }
+        return totQty;
+    }
+
+    public void findKnapsack(String[] preparedSpice) {
+        
+        
+        for (int i = 0; i < preparedSpice.length; i++) {
+            if (preparedSpice[i].contains("knapsack capacity")) {
+                preparedSpice[i] = preparedSpice[i].replaceAll("[^0-9]", "");
+            }
+        }
+        
+    
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         GreedySpice s = new GreedySpice();
         String[] a = s.spiceFinder();
-        s.findSpiceInfo(a);
+        String b = s.findSpiceInfo(a);
+        //System.out.println(s.findPrice(b));
+        //System.out.println(s.findQty(b));
+        s.findKnapsack(a);
     }
 }
