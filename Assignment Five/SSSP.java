c1e2d1af989f6abc04c840871ecacef7f2bbeedb
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-12-04 23:19:53 -0500

    graph helpers

--- Assignment Five/SSSP.java
+++ Assignment Five/SSSP.java
@@ -13,6 +13,8 @@ public class SSSP {
     ArrayList<String> edgeIntList1 = new ArrayList<String>();
     ArrayList<String> edgeIntList2 = new ArrayList<String>();
     ArrayList<Integer> vertexIntList = new ArrayList<Integer>();
+    int graph1 = 5;
+    int graph2, graph3, graph4 = 7;
     public String[] findFile() throws FileNotFoundException {
         Scanner graphScan = new Scanner(new File("graph2.txt"));
         ArrayList<String> graphList = new ArrayList<String>();
@@ -73,10 +75,72 @@ public class SSSP {
         List<String> trim2 = edgeIntList2.stream().map(String::trim).collect(Collectors.toList());
         return trim2;
     }
+
+    public List<Integer> firstGraphV(ArrayList<Integer> list){
+        List<Integer> firstGraphVL = new ArrayList<Integer>();
+        for (int i = 0; i < graph1; i++) {
+            firstGraphVL.add(list.get(i));
+        }
+        return firstGraphVL;
+    }
+
+    public List<Integer> secondGraphV(ArrayList<Integer> list) {
+        ArrayList<Integer> secondGraphVL = new ArrayList<Integer>();
+        for (int i = graph1; i < 12; i++) {
+            secondGraphVL.add(list.get(i));
+        }
+        return secondGraphVL;
+    }
+
+    public ArrayList<Integer> thirdGraphV(ArrayList<Integer> list){
+        ArrayList<Integer> thirdGraphVL = new ArrayList<Integer>();
+        for (int i = 12; i < 19; i++) {
+            thirdGraphVL.add(list.get(i));
+        }
+        return thirdGraphVL;
+    }
+
+    public ArrayList<Integer> fourthGraphV(ArrayList<Integer> list){
+        ArrayList<Integer> fourthGraphVL = new ArrayList<Integer>();
+        for (int i = 19; i < 26; i++) {
+            fourthGraphVL.add(list.get(i));
+        }
+        return fourthGraphVL;
+    }
+//========================================================================
+    public List<Integer> firstGraphE(List<String> list) {
+        List<Integer> intList = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            intList.add(Integer.valueOf(list.get(i)));
+        }
+        return intList;
+    }
+    public List<Integer> secondGraphE(List<String> list) {
+        List<Integer> intList1 = new ArrayList<Integer>();
+        for (int i = 10; i < 22; i++) {
+            intList1.add(Integer.valueOf(list.get(i)));
+        }
+        return intList1;
+    }
+    public List<Integer> thirdGraphE(List<String> list) {
+        List<Integer> intList2 = new ArrayList<Integer>();
+        for (int i = 22; i < 34; i++) {
+            intList2.add(Integer.valueOf(list.get(i)));
+        }
+        return intList2;
+    }
+    public List<Integer> fourthGraphE(List<String> list) {
+        List<Integer> intList3 = new ArrayList<Integer>();
+        for (int i = 34; i < 46; i++) {
+            intList3.add(Integer.valueOf(list.get(i)));
+        }
+        return intList3;
+    }
     public static void main(String[] args) throws FileNotFoundException {
         SSSP n = new SSSP();
         String[] a = n.findFile();
-        System.out.println(n.getEdgesWeight(a));
+        System.out.println(n.firstGraphE(n.getEdgesWeight(a)));
+        
         
     }
 }
