e8e05c2c507c7daea397fcb95e4585044cff42e5
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-12-04 23:15:11 -0500

    class init

---
+++ Assignment Five/SSSPBellmanFord.java
@@ -0,0 +1,22 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
+class SSSPBellmanFord {
+
+    class Edge {
+        int s;
+        int d; 
+        int w;
+
+        Edge() {
+            s = 0; 
+            d = 0;
+            w = 0;
+        }
+    };
+
+    int V, E;
+    Edge edge[];
+
+    
+  }
