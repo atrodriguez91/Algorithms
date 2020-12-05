b590e1042734ebea8eda9bebe010b5158a0b8228
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-12-04 23:16:06 -0500

    bellmanford traversal

--- Assignment Five/SSSPBellmanFord.java
+++ Assignment Five/SSSPBellmanFord.java
@@ -18,5 +18,31 @@ class SSSPBellmanFord {
     int V, E;
     Edge edge[];
 
-    
+    SSSPBellmanFord(int v, int e) {
+        V = v;
+        E = e;
+        edge = new Edge[e];
+        for (int i = 0; i < e; ++i)
+            edge[i] = new Edge();
+    }
+
+    public static void BellmanFordProc(SSSPBellmanFord graph, int s) {
+        int V = graph.V;
+        int E = graph.E;
+        int dist[] = new int[V];
+
+        for (int i = 0; i < V; ++i)
+            dist[i] = Integer.MAX_VALUE;
+
+        dist[s] = 0;
+
+        for (int i = 1; i < V; ++i) {
+            for (int j = 0; j < E; ++j) {
+                int u = graph.edge[j].s;
+                int v = graph.edge[j].d;
+                int w = graph.edge[j].w;
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v])
+                    dist[v] = dist[u] + w;
+            }
+        }
   }
