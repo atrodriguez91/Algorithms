ad761cd0b66a9bb14bd415887776eb8212f5aad8
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-12-08 19:20:37 -0500

    first case testing scenario

--- Semester Project/TestingPool.java
+++ Semester Project/TestingPool.java
@@ -6,5 +6,20 @@ public class TestingPool {
     int totalTests = 0; // Cumulative amount of tests for entire procedure
     boolean isInfected = false; // Flag for infection
 
-    
+ // Pooled testing function algorithm
+    public static int runPooledTest(int testingPool[]) {
+        TestingPool test = new TestingPool();
+        test.testCount = 1; // One test must be done for the initial large group
+        for (int i = 0; i < testingPool.length; i++) {
+            if (testingPool[i] == test.infected) {
+                test.testCount++;
+                test.isInfected = true;
+                System.out.println("Infection found in main group");
+                break;
+            } else {
+                test.isInfected = false;
+            }
+        }
+        
+    }
 }
