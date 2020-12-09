b5863aa8d563f3b94aded16cb31886aed7ab4b1d
atrodriguez91 <46941067+atrodriguez91@users.noreply.github.com>
2020-12-08 19:16:33 -0500

    variables init

---
+++ Semester Project/TestingPool.java
@@ -0,0 +1,10 @@
+public class TestingPool {
+    int[] testingPool; // Batch of 8 from population size
+    int infected = 1; // If a sample if infected
+    int notInfected = 0; // If a sample isn't infected
+    int testCount = 0; // Amount of tests depending on case
+    int totalTests = 0; // Cumulative amount of tests for entire procedure
+    boolean isInfected = false; // Flag for infection
+
+    
+}
